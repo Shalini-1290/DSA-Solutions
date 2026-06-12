be29cca6528513a336d1dfaaa9519a1199b13129
Shalini_codes <ug25ccs031@iiitsurat.ac.in>
2026-06-12 13:59:18 +0530

    Create 0724_Find_Pivot_Index.cpp

---
+++ Arrays/0724_Find_Pivot_Index.cpp
@@ -0,0 +1,37 @@
+/*
+  Leetcode 724-find pivot index
+
+pattern: prefix sum+ suffix sum
+
+observation:  left sum==rightsum
+
+  optimization: rightsum=totalsum-leftsum-nums[i]
+  */
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+            int n = nums.size();
+
+        vector<int> prefix(n);
+
+        prefix[0] = nums[0];
+
+        for(int i = 1; i < n; i++) {
+            prefix[i] = prefix[i - 1] + nums[i];
+        }
+
+        for(int i = 0; i < n; i++) {
+
+            int leftSum = (i == 0) ? 0 : prefix[i - 1];
+
+            int rightSum = prefix[n - 1] - prefix[i];
+
+            if(leftSum == rightSum) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+    
+};
