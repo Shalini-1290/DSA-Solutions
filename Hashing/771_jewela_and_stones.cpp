fe3fc3a4594b0fe9a703299051fb83ba9b4b2050
Shalini_codes <ug25ccs031@iiitsurat.ac.in>
2026-06-30 12:56:05 +0530

    Create 771_jewels_and_stones.cpp

---
+++ Hashing/771_jewela_and_stones.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int numJewelsInStones(string jewels, string stones) {
+        unordered_set<char>s;
+        int c=0;
+        for(int i=0;i<jewels.length();i++){
+             s.insert(jewels[i]);
+        }
+        for(int i=0;i<stones.length();i++){
+            if(s.find(stones[i])!=s.end())
+                  c++;
+        }
+
+        return c;
+    }
+};
